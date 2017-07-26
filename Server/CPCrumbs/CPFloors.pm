b8ce9feb52c77fb420be47914cad1cea1c130518
Lynx <fantasyhacking@users.noreply.github.com>
2017-07-26 06:51:49 +0530

    Add files via upload

---
+++ Server/CPCrumbs/CPFloors.pm
@@ -0,0 +1,29 @@
+package CPFloors;
+
+our %_floors = ();
+$_floors{0} = {'cost' => 20};
+$_floors{1} = {'cost' => 680};
+$_floors{2} = {'cost' => 620};
+$_floors{3} = {'cost' => 530};
+$_floors{4} = {'cost' => 530};
+$_floors{5} = {'cost' => 510};
+$_floors{6} = {'cost' => 540};
+$_floors{7} = {'cost' => 1000};
+$_floors{8} = {'cost' => 280};
+$_floors{9} = {'cost' => 370};
+$_floors{10} = {'cost' => 400};
+$_floors{11} = {'cost' => 530};
+$_floors{12} = {'cost' => 750};
+$_floors{13} = {'cost' => 620};
+$_floors{14} = {'cost' => 700};
+$_floors{15} = {'cost' => 530};
+$_floors{16} = {'cost' => 800};
+$_floors{17} = {'cost' => 530};
+$_floors{18} = {'cost' => 400};
+$_floors{19} = {'cost' => 530};
+$_floors{20} = {'cost' => 1200};
+$_floors{21} = {'cost' => 400};
+$_floors{22} = {'cost' => 530};
+$_floors{23} = {'cost' => 750};
+
+1;
